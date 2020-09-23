0f0463c1dde5bcae3ec8e0d0e7017b5bf17ada45
dguzmaneygt44 <djguzman_arena@hotmail.com>
2020-09-22 20:48:51 -0600

    Se configuró la capa de repositorio.

---
+++ backend/as2p2/src/main/java/gt/edu/umg/as2p2/repository/DoctorRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.as2p2.repository;
+
+import gt.edu.umg.as2p2.model.DoctorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author AK272DT
+ */
+@Repository
+public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {}
